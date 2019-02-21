ef63d7a61ff164a16523c4f063a6be3623acf2fa
Aronian66 <arturotoll5@gmail.com>
2019-02-21 13:57:59 -0500

    add comment to sprint

--- src/proyect/Sprint/index.js
+++ src/proyect/Sprint/index.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Sprint extends Component {
     render(){
         return(
-            <div>Componente de un sprint</div>
+            <div>Componente de un sprint
+                <p>Este es un salto de linea dentro del div principal</p>
+            </div>
         )
     }
 }
